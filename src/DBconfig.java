2b078e82607233b16c19310877bea491f350e5d6
Kushal <kushaljaipur@gmail.com>
2023-06-24 12:11:01 +0530

    Set up

--- src/DBconfig.java
+++ src/DBconfig.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -24,9 +22,14 @@ public class DBconfig {
                 query = "USE inventory;";
                 stmt.executeUpdate(query);
 
-                query = "CREATE TABLE dictionary (word varchar(255));";
+                query = "CREATE TABLE value (\n" +
+                        "  name VARCHAR(20),\n" +
+                        "  pro_ID INT(6) PRIMARY KEY,\n" +
+                        "  quantity INT(5),\n" +
+                        "  Description varchar(200)\n" +
+                        ");";
                 stmt.executeUpdate(query);
-                addWords(con);
+                addProducts(con);
             }
             else{
                 query = "USE inventory;";
@@ -39,9 +42,14 @@ public class DBconfig {
                     list.add(res.getString(1));
                 }
                 if(!list.contains("values")){
-                    query = "CREATE TABLE dictionary (word varchar(255));";
+                    query = "CREATE TABLE value (\n" +
+                            "  name VARCHAR(20),\n" +
+                            "  pro_ID INT(6) PRIMARY KEY,\n" +
+                            "  quantity INT(5),\n" +
+                            "  Description varchar(200)\n" +
+                            ");";
                     stmt.executeUpdate(query);
-                    addWords(con);
+                    addProducts(con);
                 }
             }
             res.close();
@@ -53,22 +61,33 @@ public class DBconfig {
         }
     }
 
-    private void addWords(Connection con){
-        try{
-            String filePath = "All words.txt";
-            String sql = "INSERT INTO dictionary VALUES (?);";
+    private void addProducts(Connection con) {
+        try {
+            String sql = "INSERT INTO value (name, pro_ID, quantity, Description) VALUES (?, ?, ?, ?);";
             PreparedStatement statement = con.prepareStatement(sql);
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                statement.setString(1, line);
+
+            // Sample product values
+            String[][] products = {
+                    {"Sony Camera", "100001", "50", "108MP, with detailed specifications."},
+                    {"Samsung Smart watch", "100002", "20", "An round OLED display, with heart and  oxygen rate sensor."},
+                    {"Sony Smart LED TV", "100003", "10", "75'inch 4K display, with 120Hz refresh rate."},
+                    {"Apple iPhone 12", "100004", "30", "6.1'inch OLED display, with 5G support."},
+                    {"Apple MacBook Pro", "100005", "15", "13'inch 4K display, with 16GB RAM."}
+
+            };
+
+            for (String[] product : products) {
+                statement.setString(1, product[0]);
+                statement.setInt(2, Integer.parseInt(product[1]));
+                statement.setInt(3, Integer.parseInt(product[2]));
+                statement.setString(4, product[3]);
                 statement.executeUpdate();
             }
-            reader.close();
-            System.out.println("Words inserted successfully.");
-        }
-        catch (Exception ex) {
+
+            System.out.println("Products inserted successfully.");
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
 }
